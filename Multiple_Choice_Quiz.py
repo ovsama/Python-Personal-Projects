c0dd2e494605d0999ca0eff9306f8015266ff5da
O. EL ACHHAB <104256816+ovsama@users.noreply.github.com>
2022-10-08 17:32:07 +0000

    Update Multiple_Choice_Quiz.py

--- Multiple_Choice_Quiz.py
+++ Multiple_Choice_Quiz.py
@@ -1,39 +1,47 @@
-#Building a Multiple Choice Quiz 
+#This Python program creates a simple Multiple Choice Quiz.
 
-question_prompts = [
-    "what color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
-    "what color are bananas?\n(a) Teal\n (b) Magenta\n(c) Yellow\n\n",
-    "what color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
-]
-
-
- #create a special class: Question
-
+#Create a list containing a bunch of questions for the quiz:
+question_prompts =[
+  "What color is water?\n(1) Blue\n(2) White\n(3) Colorless\n\n",                                           
+  "What color is air?\n(1) Colorless\n(2) White\n(3) Yellow\n\n", 
+  "What color is the sun?\n(1) Yellow\n(2) Colorless\n(3) White\n\n", 
+    ]
+ 
+#Create a class called 'Question':
 class Question:
- def __init__(self, prompt, answer):
-  self.prompt = prompt
-  self.answer = answer
+  def __init__(self, prompt, answer):
+    self.prompt = prompt
+    self.answer = answer
 
-
+#Create a list of 'Question class' objects in order to store an answer for each prompt:
 questions = [
-     Question(question_prompts[0], "a"),
-     Question(question_prompts[1], "c"),  
-     Question(question_prompts[2], "b") 
+     Question(question_prompts[0], "3"),
+     Question(question_prompts[1], "1"),  
+     Question(question_prompts[2], "3") 
  ]
 
 
-#Here's our program
-
-def run_test(questions):
-    score = 0
+#Create a function 'run_quiz' which allows running the quiz:
+def run_quiz(questions):
+    score=0
     for question in questions:
         answer = input(question.prompt)
         if answer == question.answer:
-            score += 1
+                score += 1
 
-    print("You got" + str(score) + "/" + str(len(questions)) + "correct")
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct") 
 
-run_test(questions)
+#Run the quiz:
+run_quiz(questions)
+
+
+
+
+
+
+
+
+
 
 
 
